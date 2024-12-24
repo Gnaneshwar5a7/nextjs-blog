2c7607a3e37cd6d2ee8feac17a011efe498973d7
Gnaneshwar Sindhe <gnaneshwar.sindhe@techigai.local>
2024-12-24 16:11:41 +0530

    session3

---
+++ src/app/fav-char/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from "react";
+
+export default function JohnWickPage() {
+  console.log("hi");
+  return (
+    <div>
+      <p>Hi I am John Wick</p>
+      <p>My favorite quote is</p>
+      <p>-- A man without purpose is nothing</p>
+    </div>
+  );
+}
