8d6bd030efba8a037e1e38e04d666d2de64b68b0
Gnaneshwar Sindhe <gnaneshwar.sindhe@techigai.local>
2025-01-16 12:22:34 +0530

    pg

--- src/db/sequelize.ts
+++ src/db/sequelize.ts
@@ -1,10 +1,10 @@
 import { Sequelize } from "sequelize";
 import { env } from "process";
+import pg from 'pg';
 
-console.log("Database URL:", process.env.DATABASE_URL, env.DATABASE_URL);
-
-export const sequelize = new Sequelize(env.DATABASE_URL || "", {
+export const sequelize = new Sequelize(env.DATABASE_URL || "postgres://neondb_owner:f50LOohIKMRy@ep-yellow-heart-a5ziyv3v-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require", {
   dialect: "postgres",
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true, // Neon requires SSL connections
