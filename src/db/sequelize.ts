4be081485d88906506f2532d4fe5d89132911e68
Gnaneshwar Sindhe <gnaneshwar.sindhe@techigai.local>
2025-01-16 11:16:29 +0530

    db

--- src/db/sequelize.ts
+++ src/db/sequelize.ts
@@ -1,7 +1,13 @@
 import { Sequelize } from "sequelize";
 import sqlite3 from "sqlite3";
+import fs from "fs";
 const databasePath =
-  process.env.NODE_ENV === "production" ? "/tmp/blog.sqlite" : "./tmp/blog.sqlite";
+  process.env.NODE_ENV === "production"
+    ? "/tmp/blog.sqlite"
+    : "./tmp/blog.sqlite";
+
+console.log(`Database path: ${databasePath}`);
+console.log("Writable directories:", fs.readdirSync("/tmp"));
 
 export const sequelize = new Sequelize({
   dialect: "sqlite",
