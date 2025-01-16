84363631a4c20ee35660c552e96b8126aabe447f
Gnaneshwar Sindhe <gnaneshwar.sindhe@techigai.local>
2025-01-16 10:45:44 +0530

    create db

--- src/db/sequelize.ts
+++ src/db/sequelize.ts
@@ -4,5 +4,5 @@ import sqlite3 from "sqlite3";
 export const sequelize = new Sequelize({
   dialect: "sqlite",
   dialectModule: sqlite3,
-  storage: "./tmp/blog.sqlite",
+  storage: "/tmp/blog.sqlite",
 });
