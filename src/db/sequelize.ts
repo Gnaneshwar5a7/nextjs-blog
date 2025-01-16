8599521965c48b86ce7df2bf678f79cf94a21682
Gnaneshwar Sindhe <gnaneshwar.sindhe@techigai.local>
2025-01-16 11:12:38 +0530

    create db

--- src/db/sequelize.ts
+++ src/db/sequelize.ts
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize";
 import sqlite3 from "sqlite3";
+const databasePath =
+  process.env.NODE_ENV === "production" ? "/tmp/blog.sqlite" : "./tmp/blog.sqlite";
 
 export const sequelize = new Sequelize({
   dialect: "sqlite",
   dialectModule: sqlite3,
-  storage: "/tmp/blog.sqlite",
+  storage: databasePath,
 });
